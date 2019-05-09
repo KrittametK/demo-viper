58ce799545e5c5a7098d616a01ae17dc485798f7
KrittametK <k.koomueang@gmail.com>
2019-05-09 11:20:19 +0700

    start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/krittamet/demo-viper
+
+go 1.12
+
+require github.com/spf13/viper v1.3.2
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Configuration struct {
+	App     AppConfiguration
+	MongoDb DatabaseConfiguration
+}
+type AppConfiguration struct {
+	Env   string
+	Port  int
+	Debug bool
+}
+type DatabaseConfiguration struct {
+	Connection string
+}
+
+func main() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	var configuration Configuration
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+	fmt.Println(configuration.App)
+	fmt.Println(configuration.MongoDb)
+}
